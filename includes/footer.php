4b98d5845f12e91b4d6d5d0d060602a00e5258b9
Siddhesh-wagh <siddhesh.01092004@gmail.com>
2025-05-04 18:29:44 +0530

    footer updataion

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,15 @@
-</div> <!-- closing the main content div -->
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Blog Post Fullstack</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
 <div class="footer-container">
     <footer>
-        © <?php echo date('Y'); ?> Blog-Post-Fullstack. All rights reserved.
+        &copy; <?php echo date('Y'); ?> Blog-Post-Fullstack. All rights reserved.<br>
+        Made by <a href="https://siddhesh-wagh.github.io/portfolio/" target="_blank">siddhesh-wagh</a>
     </footer>
 </div>
 
